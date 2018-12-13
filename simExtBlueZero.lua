b57f0507bc3234274595c3729a5bde7557f6031a
Coppelia <mfreese@gmx.ch>
2018-12-13 13:38:18 +0100

    simExtBlueZero.lua

--- simExtBlueZero.lua
+++ simExtBlueZero.lua
@@ -2,20 +2,20 @@ local simB0={}
 
 --@fun spin Call spinOnce() continuously
 --@arg string handle the node handle
-function simB0.spin(handle)
+function simB0.nodeSpin(handle)
     while sim.getSimulationState()~=sim.simulation_advancing_abouttostop do
-        simB0.spinOnce(handle)
+        simB0.nodeSpinOnce(handle)
         sim.switchThread()
     end
 end
 
 --@fun pingResolver Check if resolver node is reachable
 function simB0.pingResolver()
-    local dummyNode=simB0.create('dummyNode')
-    simB0.setAnnounceTimeout(dummyNode, 2000) -- 2 seconds timeout
-    local running=pcall(function() simB0.init(dummyNode) end)
-    if running then simB0.cleanup(dummyNode) end
-    simB0.destroy(dummyNode)
+    local dummyNode=simB0.nodeCreate('dummyNode')
+    simB0.nodeSetAnnounceTimeout(dummyNode, 2000) -- 2 seconds timeout
+    local running=pcall(function() simB0.nodeInit(dummyNode) end)
+    if running then simB0.nodeCleanup(dummyNode) end
+    simB0.nodeDestroy(dummyNode)
     return running
 end
 
