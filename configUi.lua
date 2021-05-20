f6e37ee39a0157a432e9b81351fd2724b19ca469
Federico Ferri <federico.ferri.it@gmail.com>
2021-05-20 15:35:58 +0200

    call sim.announceSceneContentChange() after generate

--- configUi.lua
+++ configUi.lua
@@ -333,6 +333,7 @@ function ConfigUI:sysCall_nonSimulation()
     if self.generatePending then --and (self.generatePending+self.generationTime)<sim.getSystemTime() then
         self.generatePending=false
         self.generateCallback(self.config)
+        sim.announceSceneContentChange()
     end
     
     -- poll for external config change:
