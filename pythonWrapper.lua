f696d03f41017465d26bdf3aee3aa87520e64a1e
Coppelia <mfreese@gmx.ch>
2022-01-07 08:55:23 +0100

    sim.moveToPose now also for Python

--- pythonWrapper.lua
+++ pythonWrapper.lua
@@ -661,7 +661,7 @@ while True:
     socket.send(cbor.dumps(rep))
 ]=]
 
-otherProg=[[
+otherProg=[=[
 
 import time
 import sys
@@ -761,24 +761,27 @@ def _switchThread():
         client.step()
 
 def _setThreadAutomaticSwitch(level):
-    global threadLocLevel
-    prev = threadLocLevel
-    if isinstance(level,bool):
-        if level == True:
-            threadLocLevel -= 1
-            if threadLocLevel < 0:
-                threadLocLevel = 0
-        if level == False:
-            threadLocLevel += 1
+    if client.threaded:
+        global threadLocLevel
+        prev = threadLocLevel
+        if isinstance(level,bool):
+            if level == True:
+                threadLocLevel -= 1
+                if threadLocLevel < 0:
+                    threadLocLevel = 0
+            if level == False:
+                threadLocLevel += 1
+        else:
+            if level >= 0:
+                threadLocLevel = level
+        if prev != threadLocLevel:
+            if threadLocLevel == 0:
+                client.setStepping(False)
+            if prev == 0 and threadLocLevel == 1:
+                client.setStepping(True)
+        return level
     else:
-        if level >= 0:
-            threadLocLevel = level
-    if prev != threadLocLevel:
-        if threadLocLevel == 0:
-            client.setStepping(False)
-        if prev == 0 and threadLocLevel == 1:
-            client.setStepping(True)
-    return level
+        return 0
     
 def print(a):
     global sim
@@ -877,6 +880,96 @@ def _moveToConfig(flags,currentPos,currentVel,currentAccel,maxVel,maxAccel,maxJe
     _setThreadAutomaticSwitch(lb)
     return outPos,outVel,outAccel,timeLeft
 
+def _moveToPose(flags,currentPoseOrMatrix,maxVel,maxAccel,maxJerk,targetPoseOrMatrix,callback,auxData=None,metric=None,timeStep=0):
+    lb = sim.setThreadAutomaticSwitch(False)
+
+    usingMatrices = (len(currentPoseOrMatrix)>=12)
+    if usingMatrices:
+        currentMatrix = currentPoseOrMatrix
+        targetMatrix = targetPoseOrMatrix
+    else:
+        currentMatrix = sim.buildMatrixQ(currentPoseOrMatrix,[currentPoseOrMatrix[3],currentPoseOrMatrix[4],currentPoseOrMatrix[5],currentPoseOrMatrix[6]])
+        targetMatrix = sim.buildMatrixQ(targetPoseOrMatrix,[targetPoseOrMatrix[3],targetPoseOrMatrix[4],targetPoseOrMatrix[5],targetPoseOrMatrix[6]])
+
+    outMatrix = sim.copyTable(currentMatrix)
+    axis,angle = sim.getRotationAxis(currentMatrix,targetMatrix)
+    timeLeft = 0
+    if metric:
+        # Here we treat the movement as a 1 DoF movement, where we simply interpolate via t between
+        # the start and goal pose. This always results in straight line movement paths
+        dx = [(targetMatrix[3]-currentMatrix[3])*metric[0],(targetMatrix[7]-currentMatrix[7])*metric[1],(targetMatrix[11]-currentMatrix[11])*metric[2],angle*metric[3]]
+        distance = math.sqrt(dx[0]*dx[0]+dx[1]*dx[1]+dx[2]*dx[2]+dx[3]*dx[3])
+        if distance > 0.000001:
+            currentPosVelAccel = [0,0,0]
+            maxVelAccelJerk = [maxVel[0],maxAccel[0],maxJerk[0]]
+            targetPosVel = [distance,0]
+            ruckigObject = sim.ruckigPos(1,0.0001,flags,currentPosVelAccel,maxVelAccelJerk,[1],targetPosVel)
+            result = 0
+            while result == 0:
+                dt = timeStep
+                if dt == 0:
+                    dt = sim.getSimulationTimeStep()
+                result,newPosVelAccel,syncTime = sim.ruckigStep(ruckigObject,dt)
+                if result >= 0:
+                    if result == 0:
+                        timeLeft = dt-syncTime
+                    t = newPosVelAccel[0]/distance
+                    mi = sim.interpolateMatrices(currentMatrix,targetMatrix,t)
+                    nv = [newPosVelAccel[1]]
+                    na = [newPosVelAccel[2]]
+                    if not usingMatrices:
+                        q = sim.getQuaternionFromMatrix(mi)
+                        mi = [mi[3],mi[7],mi[11],q[0],q[1],q[2],q[3]]
+                    callback(mi,nv,na,auxData)
+                else:
+                    raise RuntimeError("sim.ruckigStep returned error code "+result)
+                if result == 0:
+                    sim.switchThread()
+            sim.ruckigRemove(ruckigObject)
+    else:
+        # Here we treat the movement as a 4 DoF movement, where each of X, Y, Z and rotation
+        # is handled and controlled individually. This can result in non-straight line movement paths,
+        # due to how the Ruckig functions operate depending on 'flags'
+        dx = [targetMatrix[3]-currentMatrix[3],targetMatrix[7]-currentMatrix[7],targetMatrix[11]-currentMatrix[11],angle]
+        currentPosVelAccel = [0,0,0,0,0,0,0,0,0,0,0,0]
+        maxVelAccelJerk = [maxVel[0],maxVel[1],maxVel[2],maxVel[3],maxAccel[0],maxAccel[1],maxAccel[2],maxAccel[3],maxJerk[0],maxJerk[1],maxJerk[2],maxJerk[3]]
+        targetPosVel = [dx[0],dx[1],dx[2],dx[3],0,0,0,0,0]
+        ruckigObject = sim.ruckigPos(4,0.0001,flags,currentPosVelAccel,maxVelAccelJerk,[1,1,1,1],targetPosVel)
+        result = 0
+        while result == 0:
+            dt = timeStep
+            if dt == 0:
+                dt = sim.getSimulationTimeStep()
+            result,newPosVelAccel,syncTime = sim.ruckigStep(ruckigObject,dt)
+            if result >= 0:
+                if result == 0:
+                    timeLeft = dt-syncTime
+                t = 0
+                if abs(angle)>math.pi*0.00001:
+                    t = newPosVelAccel[3]/angle
+                mi = sim.interpolateMatrices(currentMatrix,targetMatrix,t)
+                mi[3] = currentMatrix[3]+newPosVelAccel[0]
+                mi[7] = currentMatrix[7]+newPosVelAccel[1]
+                mi[11] = currentMatrix[11]+newPosVelAccel[2]
+                nv = [newPosVelAccel[4],newPosVelAccel[5],newPosVelAccel[6],newPosVelAccel[7]]
+                na = [newPosVelAccel[8],newPosVelAccel[9],newPosVelAccel[10],newPosVelAccel[11]]
+                if not usingMatrices:
+                    q = sim.getQuaternionFromMatrix(mi)
+                    mi = [mi[3],mi[7],mi[11],q[0],q[1],q[2],q[3]]
+                callback(mi,nv,na,auxData)
+            else:
+                raise RuntimeError("sim.ruckigStep returned error code "+result)
+            if result == 0:
+                sim.switchThread()
+        sim.ruckigRemove(ruckigObject)
+
+    if not usingMatrices:
+        q = sim.getQuaternionFromMatrix(outMatrix)
+        outMatrix = [outMatrix[3],outMatrix[7],outMatrix[11],q[0],q[1],q[2],q[3]]
+
+    sim.setThreadAutomaticSwitch(lb)
+    return outMatrix,timeLeft
+
 
 def __startClientScript__():
     global client
@@ -897,10 +990,12 @@ def __startClientScript__():
     sim.wait = _wait
     sim.waitForSignal = _waitForSignal
     sim.moveToConfig = _moveToConfig
+    sim.moveToPose = _moveToPose
     threadFunc = _getFuncIfExists("threadMain")
-    args=None
+    args = None
     if threadFunc==None:
         # Run as 'non-threaded'
+        client.threaded = False
         funcToRun = "sysCall_init"
         func=_getFuncIfExists(funcToRun)
         if func:
@@ -939,4 +1034,4 @@ def __startClientScript__():
         finally:
             client.setStepping(False)
             client.call('serviceCall', ["threadEnded"])
-]]
+]=]
