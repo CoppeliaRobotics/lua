a38f773e7f8456190bc3556a7cceb1994bd0401f
Federico Ferri <federico.ferri.it@gmail.com>
2023-01-20 17:29:28 +0100

    getMesh() moved to simIGL.lua

--- addon_booleanMesh.lua
+++ addon_booleanMesh.lua
@@ -4,18 +4,11 @@ function sysCall_init()
         if #sel~=2 or sim.getObjectType(sel[1])~=sim.object_shape_type or sim.getObjectType(sel[2])~=sim.object_shape_type then
             sim.addLog(sim.verbosity_scripterrors,'This tool requires exactly two shapes to be selected.')
         else
-            local m=simIGL.meshBoolean(getMesh(sel[1]),getMesh(sel[2]),op())
+            local m=simIGL.meshBoolean(simIGL.getMesh(sel[1]),simIGL.getMesh(sel[2]),op())
             local h=sim.createMeshShape(3,math.pi/8,m.vertices,m.indices)
         end
     else
         sim.addLog(sim.verbosity_scripterrors,'This tool requires the IGL plugin.')
     end
     return {cmd='cleanup'}
-end
-
-function getMesh(h)
-    local v,i,n=sim.getShapeMesh(h)
-    local m=sim.getObjectMatrix(h,-1)
-    v=sim.multiplyVector(m,v)
-    return {vertices=v, indices=i}
 end
