7fcec978123d770c1318858dd6576981ad056157
Federico Ferri <federico.ferri.it@gmail.com>
2023-01-27 16:06:09 +0100

    addLog->msgBox

--- addon_booleanMesh.lua
+++ addon_booleanMesh.lua
@@ -2,13 +2,13 @@ function sysCall_init()
     if sim.isPluginLoaded('IGL') then
         local sel=sim.getObjectSelection()
         if #sel~=2 or sim.getObjectType(sel[1])~=sim.object_shape_type or sim.getObjectType(sel[2])~=sim.object_shape_type then
-            sim.addLog(sim.verbosity_scripterrors,'This tool requires exactly two shapes to be selected.')
+            simUI.msgBox(simUI.msgbox_type.critical,simUI.msgbox_buttons.ok,'Mesh boolean add-on','This tool requires exactly two shapes to be selected.')
         else
             local m=simIGL.meshBoolean(simIGL.getMesh(sel[1]),simIGL.getMesh(sel[2]),op())
             local h=sim.createMeshShape(3,math.pi/8,m.vertices,m.indices)
         end
     else
-        sim.addLog(sim.verbosity_scripterrors,'This tool requires the IGL plugin.')
+        simUI.msgBox(simUI.msgbox_type.critical,simUI.msgbox_buttons.ok,'Mesh boolean add-on','This tool requires the IGL plugin.')
     end
     return {cmd='cleanup'}
 end
