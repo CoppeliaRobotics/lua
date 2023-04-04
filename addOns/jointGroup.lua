4eb2fabfb033df9e957ef6c1912b4b26ff55cfff
Federico Ferri <federico.ferri.it@gmail.com>
2023-04-04 18:05:05 +0200

    jointGroup addon funcs

---
+++ addOns/jointGroup.lua
@@ -0,0 +1,10 @@
+function getJointGroups(modelHandle)
+    local ret={}
+    sim.visitTree(modelHandle,function(h)
+        if h~=modelHandle and sim.getModelProperty(h)&sim.modelproperty_not_model==0 then return false end
+        if sim.readCustomDataBlock(h,'__jointGroup__') then
+            table.insert(ret,h)
+        end
+    end)
+    return ret
+end
