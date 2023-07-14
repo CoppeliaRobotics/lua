25be26942581050c605c00d9b5efdb1c1b7189e6
Federico Ferri <federico.ferri.it@gmail.com>
2023-07-14 12:02:02 +0200

    add info func

--- python-client-bridge.lua
+++ python-client-bridge.lua
@@ -1,4 +1,6 @@
-function __call(b)
+pythonClientBridge={}
+
+function pythonClientBridge.call(b)
     cbor=require'org.conman.cbor'
     i=cbor.decode(b)
     require'var'
@@ -6,4 +8,29 @@ function __call(b)
     local ok,r=pcall(function() return {f(table.unpack(i.args))} end)
     if ok then r={success=true,result=r} else r={success=false,error=r} end
     return cbor.encode(r)
+end
+
+function pythonClientBridge.info(obj)
+    if type(obj)=='string' then obj=pythonClientBridge.getField(obj) end
+    if type(obj)~='table' then return obj end
+    local ret={}
+    for k,v in pairs(obj) do
+        if type(v)=='table' then
+            ret[k]=pythonClientBridge.info(v)
+        elseif type(v)=='function' then
+            ret[k]={func={}}
+        elseif type(v)~='function' then
+            ret[k]={const=v}
+        end
+    end
+    return ret
+end
+
+function pythonClientBridge.getField(f)
+    local v=_G
+    for w in string.gmatch(f,'[%w_]+') do
+        v=v[w]
+        if not v then return nil end
+    end
+    return v
 end
