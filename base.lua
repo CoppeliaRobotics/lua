9e5442e7555b2a4f3fc47b782241b34cb58626f5
Federico Ferri <federico.ferri.it@gmail.com>
2023-10-03 11:21:50 +0200

    + rerequire()

--- base.lua
+++ base.lua
@@ -40,6 +40,38 @@ function require(...)
     return table.unpack(retVals)
 end
 
+function rerequire(name)
+    local searchPaths = string.split(package.path, package.config:sub(3, 3))
+    local nameWithSlashes = name:gsub('%.', '/')
+    for _, searchPath in ipairs(searchPaths) do
+        local exists = false
+        local fileName = searchPath:gsub('?', nameWithSlashes)
+        local f = io.open(fileName, 'r')
+        if f ~= nil then
+            exists = true
+            io.close(f)
+        end
+        if exists then
+            local success, result = xpcall(
+                function(filename, env)
+                    local f = assert(loadfile(filename))
+                    return f()
+                end,
+                function(err)
+                    return debug.traceback(err)
+                end,
+                fileName
+            )
+            if success then
+                return result
+            else
+                sim.addLog(sim.verbosity_errors, result)
+                return
+            end
+        end
+    end
+end
+
 _S.pcall=pcall
 function pcall(...)
     local fl=setYieldAllowed(false) -- important when called from coroutine
