1b5b8d03d3c10aa4fb4d2c193ca0b81dfad033f9
Coppelia <mfreese@gmx.ch>
2024-05-22 11:35:14 +0200

    deprecated functions with simple, one-time warning output

--- sim-deprecated.lua
+++ sim-deprecated.lua
@@ -1,8 +1,8 @@
 local deprecated = {}
 
 deprecated.functions = {
-    {'sim.addBanner', ''},
-    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'addBanner', ''},
+    {'readCustomDataBlock', 'readCustomStringData'},
     {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
     {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
     {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
@@ -43,15 +43,19 @@ function deprecated.extend(sim)
     for _, pair in ipairs(deprecated.functions) do
         local old, new = table.unpack(pair)
         if new == '' then
-            sim[old] = function(...)
-                sim.addLog(sim.verbosity_warnings | sim.verbosity_once, string.format('sim.%s is deprecated and the related functionality will disappear in a future release.', old, new))
-                return sim[new](...)
-            end
+            sim[old] = wrap(sim[old], function(origFunc)
+                return function(...)
+                    sim.addLog(sim.verbosity_warnings | sim.verbosity_once, string.format('sim.%s is deprecated and the related functionality will disappear in a future release.', old, new))
+                    origFunc(...)
+                end
+            end)
         else
-            sim[old] = function(...)
-                sim.addLog(sim.verbosity_warnings | sim.verbosity_once, string.format('sim.%s is deprecated. please use sim.%s instead.', old, new))
-                return sim[new](...)
-            end
+            sim[old] = wrap(sim[old], function(origFunc)
+                return function(...)
+                    sim.addLog(sim.verbosity_warnings | sim.verbosity_once, string.format('sim.%s is deprecated. please use sim.%s instead.', old, new))
+                    origFunc(...)
+                end
+            end)
         end
     end
 end
