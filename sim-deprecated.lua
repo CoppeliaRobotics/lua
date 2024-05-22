ad8669f8737c507743cfdb52477316770e6b7e14
Coppelia <mfreese@gmx.ch>
2024-05-22 10:39:39 +0200

    sim.getReferencedHandlesTags

--- sim-deprecated.lua
+++ sim-deprecated.lua
@@ -1,16 +1,57 @@
 local deprecated = {}
 
 deprecated.functions = {
-    {'oldFunction1', 'newFunction1'},
-    {'oldFunction2', 'newFunction2'},
+    {'sim.addBanner', ''},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
+    {'xxxxxxxxxxx', 'xxxxxxxxxxxx'},
 }
 
 function deprecated.extend(sim)
     for _, pair in ipairs(deprecated.functions) do
         local old, new = table.unpack(pair)
-        sim[old] = function(...)
-            sim.addLog(sim.verbosity_warnings | sim.verbosity_once, string.format('sim.%s is deprecated. please use sim.%s instead.', old, new))
-            return sim[new](...)
+        if new == '' then
+            sim[old] = function(...)
+                sim.addLog(sim.verbosity_warnings | sim.verbosity_once, string.format('sim.%s is deprecated and the related functionality will disappear in a future release.', old, new))
+                return sim[new](...)
+            end
+        else
+            sim[old] = function(...)
+                sim.addLog(sim.verbosity_warnings | sim.verbosity_once, string.format('sim.%s is deprecated. please use sim.%s instead.', old, new))
+                return sim[new](...)
+            end
         end
     end
 end
