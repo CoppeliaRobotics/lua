66a01183012af7ea51d6ad2f7ce9ea29941e3979
Federico Ferri <federico.ferri.it@gmail.com>
2024-05-28 10:10:47 +0200

    new-style scripts change

--- models/robotConfigPath.lua
+++ models/robotConfigPath.lua
@@ -3,37 +3,27 @@ sim = require 'sim'
 local robotConfigPath = {}
 
 function robotConfigPath.create(pathMtx, parent, jointGroupPath)
-    local pathDummy = sim.createDummy(0.05)
-    sim.setObjectAlias(pathDummy, 'Path')
-    sim.setModelProperty(pathDummy, 0)
-    sim.setObjectParent(pathDummy, parent)
-    sim.setObjectInt32Param(pathDummy, sim.objintparam_visibility_layer, 0)
-    sim.setObjectInt32Param(pathDummy, sim.objintparam_manipulation_permissions, 0)
-    sim.setObjectProperty(pathDummy, sim.objectproperty_collapsed)
-    sim.setObjectPose(pathDummy, {0, 0, 0, 0, 0, 0, 1}, sim.handle_parent)
-    local s = sim.addScript(sim.scripttype_customizationscript)
-    sim.setScriptStringParam(
-        s, sim.scriptstringparam_text, [[require 'models.robotConfigPath_customization']]
-    )
-    sim.associateScriptWithObject(s, pathDummy)
-    sim.writeCustomTableData(pathDummy, 'path', pathMtx:totable())
-    local stateDummy = sim.createDummy(0.01)
-    sim.setObjectAlias(stateDummy, 'State')
-    sim.setObjectParent(stateDummy, pathDummy, false)
-    sim.setObjectPose(stateDummy, {0, 0, 0, 0, 0, 0, 1}, pathDummy)
-    sim.setObjectInt32Param(stateDummy, sim.objintparam_visibility_layer, 0)
-    sim.setObjectInt32Param(stateDummy, sim.objintparam_manipulation_permissions, 0)
-    sim.setObjectProperty(stateDummy, sim.objectproperty_collapsed)
-    local s = sim.addScript(sim.scripttype_customizationscript)
-    sim.setScriptStringParam(
-        s, sim.scriptstringparam_text, [[require 'models.robotConfig_customization'
+    local pathScript = sim.createScript(sim.scripttype_customizationscript, [[require 'models.robotConfigPath_customization']])
+    sim.setObjectAlias(pathScript, 'Path')
+    sim.setModelProperty(pathScript, 0)
+    sim.setObjectParent(pathScript, parent)
+    sim.setObjectInt32Param(pathScript, sim.objintparam_visibility_layer, 0)
+    sim.setObjectInt32Param(pathScript, sim.objintparam_manipulation_permissions, 0)
+    sim.setObjectProperty(pathScript, sim.objectproperty_collapsed)
+    sim.setObjectPose(pathScript, {0, 0, 0, 0, 0, 0, 1}, sim.handle_parent)
+    sim.writeCustomTableData(pathScript, 'path', pathMtx:totable())
+    local stateScript = sim.createScript(sim.scripttype_customizationscript, [[require 'models.robotConfig_customization'
 model = sim.getObject '::'
 color = {1, 1, 0}
 ik = false
 jointGroupPath = ']] .. jointGroupPath .. [['
-]]
-    )
-    sim.associateScriptWithObject(s, stateDummy)
+]])
+    sim.setObjectAlias(stateScript, 'State')
+    sim.setObjectParent(stateScript, pathScript, false)
+    sim.setObjectPose(stateScript, {0, 0, 0, 0, 0, 0, 1}, pathScript)
+    sim.setObjectInt32Param(stateScript, sim.objintparam_visibility_layer, 0)
+    sim.setObjectInt32Param(stateScript, sim.objintparam_manipulation_permissions, 0)
+    sim.setObjectProperty(stateScript, sim.objectproperty_collapsed)
 end
 
 return robotConfigPath
---
+++ models/robotConfig_customization-2.lua
@@ -0,0 +1,151 @@
+sim = require 'sim'
+
+function sysCall_init()
+    self = sim.getObject '.'
+    model = model or sim.getObject ':'
+    color = color or {1, 0, 0}
+    transparency = transparency or 0.5
+    ik = ik == nil and true or ik
+    removeModelClone()
+    if create then createModelClone() end
+end
+
+function sysCall_cleanup()
+    removeModelClone()
+end
+
+function sysCall_userConfig()
+    if clonedModel then
+        removeModelClone()
+    else
+        createModelClone()
+    end
+end
+
+function hasModelClone()
+    return not not clonedModel
+end
+
+function removeModelClone()
+    saveConfig()
+    saveIkTarget()
+    sim.removeReferencedObjects(self)
+    clonedModel = nil
+end
+
+function createModelClone()
+    if clonedModel then removeModelClone() end
+    local objects = {}
+    sim.visitTree(
+        model, function(handle)
+            local parent = sim.getObjectParent(handle)
+            local alias = sim.getObjectAlias(handle)
+            if parent == model and alias == 'MotionPlanning' then return false end
+            if parent == model and alias == 'Path' then return false end
+            if parent == model and not ik and alias == 'IK' then return false end
+            table.insert(objects, handle)
+        end
+    )
+    local clonedObjects = sim.copyPasteObjects(objects, 16 + 32)
+    clonedModel = clonedObjects[1]
+    sim.setReferencedHandles(self, clonedObjects)
+    local scriptsToInit = {}
+    for _, handle in ipairs(clonedObjects) do
+        local parent = sim.getObjectParent(handle)
+        local alias = sim.getObjectAlias(handle)
+        if sim.getObjectType(handle) == sim.object_script_type then
+            if parent == clonedModel and alias == 'IK' then
+                table.insert(scriptsToInit, handle)
+            else
+                local _a = sim.readCustomStringData(handle, '__jointGroup__')
+                if parent == clonedModel and _a and #_a > 0 then
+                    table.insert(scriptsToInit, handle)
+                else
+                    sim.removeObjects{handle}
+                end
+            end
+        elseif sim.getObjectType(handle) == sim.object_shape_type then
+            sim.setObjectProperty(handle, sim.objectproperty_selectinvisible)
+            sim.setObjectInt32Param(handle, sim.shapeintparam_respondable, 0)
+            sim.setObjectInt32Param(handle, sim.shapeintparam_static, 1)
+            sim.setObjectInt32Param(handle, sim.shapeintparam_culling, 1)
+            sim.setShapeColor(handle, nil, sim.colorcomponent_ambient_diffuse, color)
+            sim.setShapeColor(handle, nil, sim.colorcomponent_transparency, {transparency})
+        end
+    end
+    sim.setObjectParent(clonedModel, self, true)
+    sim.setModelProperty(
+        clonedModel, sim.getModelProperty(clonedModel) & ~sim.modelproperty_not_model
+    )
+    sim.setObjectProperty(self, sim.objectproperty_collapsed)
+    for _, scriptHandle in ipairs(scriptsToInit) do sim.initScript(scriptHandle) end
+    restoreConfig()
+    restoreIkTarget()
+    local ikObj = ObjectProxy('./IK', clonedModel)
+    if ikObj then
+        local target = ikObj:getTarget()
+        sim.setObjectSelection {target}
+    end
+end
+
+function getConfig()
+    if clonedModel then
+        local jointGroup = ObjectProxy(jointGroupPath, clonedModel)
+        return jointGroup:getConfig()
+    else
+        return sim.readCustomTableData(self, 'config')
+    end
+end
+
+function setConfig(cfg)
+    if clonedModel then
+        local jointGroup = ObjectProxy(jointGroupPath, clonedModel)
+        return jointGroup:setConfig(cfg)
+    end
+end
+
+function saveConfig()
+    if clonedModel then sim.writeCustomTableData(self, 'config', getConfig()) end
+end
+
+function restoreConfig()
+    if clonedModel then
+        local cfg = sim.readCustomTableData(self, 'config')
+        if #cfg == 0 then return end
+        setConfig(cfg)
+    end
+end
+
+function saveIkTarget()
+    if clonedModel then
+        local ikObj = ObjectProxy('./IK', clonedModel)
+        if ikObj then
+            local target = ikObj:getTarget()
+            local pose = sim.getObjectPose(target, clonedModel)
+            sim.writeCustomTableData(self, 'ikTargetPose', pose)
+        end
+    end
+end
+
+function restoreIkTarget(pose)
+    if clonedModel then
+        local ikObj = ObjectProxy('./IK', clonedModel)
+        if ikObj then
+            local target = ikObj:getTarget()
+            local pose = sim.readCustomTableData(self, 'ikTargetPose')
+            if #pose == 0 then return end
+            sim.setObjectPose(target, pose, clonedModel)
+        end
+    end
+end
+
+function reset()
+    sim.writeCustomBufferData(self, 'config', '')
+    sim.writeCustomBufferData(self, 'ikTargetPose', '')
+end
+
+function ObjectProxy(path, proxy, t)
+    t = t or sim.scripttype_customizationscript
+    local o = sim.getObject(path, {proxy = proxy, noError = true})
+    if o ~= -1 then return sim.getScriptFunctions(sim.getScript(t, o)) end
+end
