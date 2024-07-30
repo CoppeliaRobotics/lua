f22356fb2f297dc15865c7e14f4cf98cc1aa5940
Federico Ferri <federico.ferri.it@gmail.com>
2024-07-30 10:41:04 +0200

    stable insertion order

--- priorityqueue.lua
+++ priorityqueue.lua
@@ -3,12 +3,13 @@ priorityqueue = {}
 priorityqueue.__index = priorityqueue
 
 function priorityqueue:push(priority, value)
-    table.insert(self.heap, {priority, value})
+    self.counter = self.counter + 1
+    table.insert(self.heap, {priority, value, self.counter})
 
     local function siftUp(heap, index)
         while index > 1 do
             local parentIndex = math.floor(index / 2)
-            if heap[parentIndex][1] <= heap[index][1] then
+            if self:_compare(heap[parentIndex], heap[index]) then
                 break
             end
             heap[parentIndex], heap[index] = heap[index], heap[parentIndex]
@@ -33,10 +34,10 @@ function priorityqueue:pop()
             local right = left + 1
             local smallest = index
 
-            if left <= size and heap[left][1] < heap[smallest][1] then
+            if left <= size and not self:_compare(heap[smallest], heap[left]) then
                 smallest = left
             end
-            if right <= size and heap[right][1] < heap[smallest][1] then
+            if right <= size and not self:_compare(heap[smallest], heap[right]) then
                 smallest = right
             end
             if smallest == index then
@@ -66,11 +67,11 @@ function priorityqueue:size()
     return #self.heap
 end
 
-function priorityqueue:cancel(value, compareFn)
-    compareFn = compareFn or function(a, b) return a == b end
+function priorityqueue:cancel(value)
     local index = nil
+    local fv = type(value) == 'function'
     for i, v in ipairs(self.heap) do
-        if compareFn(v[2], value) then
+        if (fv and value(v[2])) or (not fv and v[2] == value) then
             index = i
             break
         end
@@ -81,46 +82,58 @@ function priorityqueue:cancel(value, compareFn)
     self.heap[index], self.heap[#self.heap] = self.heap[#self.heap], self.heap[index]
     table.remove(self.heap)
 
-    local function siftDown(heap, index)
-        local size = #heap
-        while 2 * index <= size do
-            local left = 2 * index
-            local right = left + 1
-            local smallest = index
+    if index <= #self.heap then
+        local function siftDown(heap, index)
+            local size = #heap
+            while 2 * index <= size do
+                local left = 2 * index
+                local right = left + 1
+                local smallest = index
 
-            if left <= size and heap[left][1] < heap[smallest][1] then
-                smallest = left
+                if left <= size and not self:_compare(heap[smallest], heap[left]) then
+                    smallest = left
+                end
+                if right <= size and not self:_compare(heap[smallest], heap[right]) then
+                    smallest = right
+                end
+                if smallest == index then
+                    break
+                end
+                heap[smallest], heap[index] = heap[index], heap[smallest]
+                index = smallest
             end
-            if right <= size and heap[right][1] < heap[smallest][1] then
-                smallest = right
-            end
-            if smallest == index then
-                break
-            end
-            heap[smallest], heap[index] = heap[index], heap[smallest]
-            index = smallest
         end
-    end
 
-    local function siftUp(heap, index)
-        while index > 1 do
-            local parentIndex = math.floor(index / 2)
-            if heap[parentIndex][1] <= heap[index][1] then
-                break
+        local function siftUp(heap, index)
+            while index > 1 do
+                local parentIndex = math.floor(index / 2)
+                if self:_compare(heap[parentIndex], heap[index]) then
+                    break
+                end
+                heap[parentIndex], heap[index] = heap[index], heap[parentIndex]
+                index = parentIndex
             end
-            heap[parentIndex], heap[index] = heap[index], heap[parentIndex]
-            index = parentIndex
         end
-    end
 
-    siftDown(self.heap, index)
-    siftUp(self.heap, index)
+        siftDown(self.heap, index)
+        siftUp(self.heap, index)
+    end
     return true
+end
+
+function priorityqueue:_compare(a, b)
+    if a[1] < b[1] then
+        return true
+    elseif a[1] == b[1] then
+        return a[3] <= b[3]
+    else
+        return false
+    end
 end
 
 setmetatable(priorityqueue, {
     __call = function(self)
-        return setmetatable({heap = {}}, self)
+        return setmetatable({heap = {}, counter = 0}, self)
     end,
 })
 
@@ -131,14 +144,18 @@ if arg and #arg == 1 and arg[1] == 'test' then
     pq:push(3, "Task 3")
     pq:push(4, "Task 4")
     pq:push(2, "Task 2")
+    pq:push(2, "Task 2b")
     assert("Task 1" == pq:peek())
     assert("Task 1" == pq:pop())
     assert("Task 2" == pq:peek())
-    assert(4 == pq:size())
+    assert(5 == pq:size())
     assert(false == pq:isempty())
     assert(true == pq:cancel("Task 3"))
-    assert(3 == pq:size())
+    assert(4 == pq:size())
     assert("Task 2" == pq:peek())
+    assert("Task 2" == pq:pop())
+    assert("Task 2b" == pq:pop())
+    assert("Task 4" == pq:peek())
     print(debug.getinfo(1, 'S').source, 'tests passed')
 end
 
