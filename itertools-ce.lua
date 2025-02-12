347d6c1753b0711016c5f03f4fed639b64ad1940
Federico Ferri <federico.ferri.it@gmail.com>
2025-02-12 11:02:19 +0100

    add combinatorics

--- itertools-ce.lua
+++ itertools-ce.lua
@@ -1,4 +1,8 @@
 registerCodeEditorInfos('itertools', [[
 function chainedIter = itertools.chain(function i1, function i2, ...)
 function chainedIter = itertools.ichain(function i1, function i2, ...)
+table p = itertools.product(table table_of_tables)
+table p = itertools.permutations(table t, int length)
+table p = itertools.combinations(table t, int length)
+table p = itertools.combinations_with_replacement(table t, int length)
 ]])
--- itertools.lua
+++ itertools.lua
@@ -44,4 +44,96 @@ function itertools.ichain(...)
     end
 end
 
+function itertools.product(arrays, current, results, index)
+    -- Example usage:
+    -- local p = itertools.product({{1, 2}, {"a", "b"}})
+
+    current = current or {}
+    results = results or {}
+    index = index or 1
+
+    if index > #arrays then
+        table.insert(results, {table.unpack(current)})
+        return results
+    end
+
+    for _, value in ipairs(arrays[index]) do
+        current[index] = value
+        itertools.product(arrays, current, results, index + 1)
+    end
+
+    return results
+end
+
+function itertools.permutations(arr, length, current, used, results)
+    -- Example usage:
+    -- local perm = itertools.permutations({1, 2, 3}, 2)
+
+    length = length or #arr
+    current = current or {}
+    used = used or {}
+    results = results or {}
+
+    if #current == length then
+        table.insert(results, {table.unpack(current)})
+        return results
+    end
+
+    for i, value in ipairs(arr) do
+        if not used[i] then
+            used[i] = true
+            table.insert(current, value)
+            itertools.permutations(arr, length, current, used, results)
+            table.remove(current)
+            used[i] = false
+        end
+    end
+
+    return results
+end
+
+function itertools.combinations(arr, length, start, current, results)
+    -- Example usage:
+    -- local comb = itertools.combinations({1, 2, 3}, 2)
+
+    start = start or 1
+    current = current or {}
+    results = results or {}
+
+    if #current == length then
+        table.insert(results, {table.unpack(current)})
+        return results
+    end
+
+    for i = start, #arr do
+        table.insert(current, arr[i])
+        itertools.combinations(arr, length, i + 1, current, results)
+        table.remove(current)
+    end
+
+    return results
+end
+
+function itertools.combinations_with_replacement(arr, length, start, current, results)
+    -- Example usage:
+    -- local comb_wr = itertools.combinations_with_replacement({1, 2, 3}, 2)
+
+    start = start or 1
+    current = current or {}
+    results = results or {}
+
+    if #current == length then
+        table.insert(results, {table.unpack(current)})
+        return results
+    end
+
+    for i = start, #arr do
+        table.insert(current, arr[i])
+        itertools.combinations_with_replacement(arr, length, i, current, results)
+        table.remove(current)
+    end
+
+    return results
+end
+
 return itertools
