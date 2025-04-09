6d9eb1699f59405c624ab300329ea87da916d533
Federico Ferri <federico.ferri.it@gmail.com>
2025-04-09 15:33:16 +0200

    lfsx: + pathsep(), pathjoin()

--- lfsx.lua
+++ lfsx.lua
@@ -16,14 +16,22 @@ function lfs.rmdir_r(dir)
     return lfs.rmdir(dir)
 end
 
+function lfs.pathsep()
+    return package.config:sub(1,1)
+end
+
+function lfs.pathjoin(parts)
+    return table.join(parts, lfs.pathsep())
+end
+
 function lfs.basename(str)
-    local name = string.gsub(str, '(.*/)(.*)', '%2')
+    local name = string.gsub(str, '(.*' .. lfs.pathsep() .. ')(.*)', '%2')
     return name
 end
 
 function lfs.dirname(str)
     if str:match('.-/.-') then
-        local name = string.gsub(str, '(.*/)(.*)', '%1')
+        local name = string.gsub(str, '(.*)(' .. lfs.pathsep() .. ')(.*)', '%1')
         return name
     else
         return ''
