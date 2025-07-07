33a4fb0a35f52797744bf6e5dfba017454bfeaab
Federico Ferri <federico.ferri.it@gmail.com>
2025-07-07 16:19:10 +0200

    cbordump

---
+++ cbordump.lua
@@ -0,0 +1,9 @@
+function cbordump(b)
+    local simSubprocess = require 'simSubprocess'
+    local ec, out = simSubprocess.exec('cbor2pretty.rb', {}, b)
+    if ec == 0 then
+        print(out)
+    else
+        error 'cbor2pretty.rb not found'
+    end
+end
