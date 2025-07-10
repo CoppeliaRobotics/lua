73aa64cfadeeae100f8a54d324efd33239280c19
Federico Ferri <federico.ferri.it@gmail.com>
2025-07-10 11:41:27 +0200

    HSV color support

--- color.lua
+++ color.lua
@@ -14,6 +14,46 @@ function Color:blue()
     return math.max(0, math.min(1, self.b))
 end
 
+function Color:hue()
+    local r, g, b = self:red(), self:green(), self:blue()
+    local maxc = math.max(r, g, b)
+    local minc = math.min(r, g, b)
+    local d = maxc - minc
+
+    if d == 0 then
+        return 0 -- Undefined hue, grayscale
+    end
+
+    local h
+    if maxc == r then
+        h = (g - b) / d
+        if g < b then h = h + 6 end
+    elseif maxc == g then
+        h = (b - r) / d + 2
+    elseif maxc == b then
+        h = (r - g) / d + 4
+    end
+
+    h = h / 6
+    return h
+end
+
+function Color:saturation()
+    local r, g, b = self:red(), self:green(), self:blue()
+    local maxc = math.max(r, g, b)
+    local minc = math.min(r, g, b)
+    if maxc == 0 then
+        return 0
+    else
+        return (maxc - minc) / maxc
+    end
+end
+
+function Color:value()
+    local r, g, b = self:red(), self:green(), self:blue()
+    return math.max(r, g, b)
+end
+
 function Color:r8()
     return math.min(255, math.floor(self:red() * 256))
 end
@@ -85,6 +125,36 @@ function Color:__tocbor(sref, stref)
     local cbor_c = require 'org.conman.cbor_c'
     return cbor_c.encode(0xC0, 4008)
         .. cbor.encode(self:rgb888())
+end
+
+function Color:rgb(r, g, b)
+    assert(self == Color, 'class method')
+    for k, v in pairs{red = r, green = g, blue = b} do
+        assert(type(v) == 'number' and v >= 0 and v <= 1, k .. ' component must be a number [0..1]')
+    end
+    return Color{r, g, b}
+end
+
+function Color:hsv(h, s, v)
+    assert(self == Color, 'class method')
+    for k, w in pairs{hue = h, saturation = s, value = v} do
+        assert(type(w) == 'number' and w >= 0 and w <= 1, k .. ' component must be a number [0..1]')
+    end
+    local r, g, b
+    local i = math.floor(h * 6)
+    local f = h * 6 - i
+    local p = v * (1 - s)
+    local q = v * (1 - f * s)
+    local t = v * (1 - (1 - f) * s)
+    i = i % 6
+    if i == 0 then r, g, b = v, t, p
+    elseif i == 1 then r, g, b = q, v, p
+    elseif i == 2 then r, g, b = p, v, t
+    elseif i == 3 then r, g, b = p, q, v
+    elseif i == 4 then r, g, b = t, p, v
+    elseif i == 5 then r, g, b = v, p, q
+    end
+    return Color{r, g, b}
 end
 
 setmetatable(
