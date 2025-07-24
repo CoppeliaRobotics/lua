8e21fbe232d00201bf1f1daf3ff123c238ffaa0d
Federico Ferri <federico.ferri.it@gmail.com>
2025-07-24 19:00:28 +0200

    refactor

--- tools/generate_typewrappers.py
+++ tools/generate_typewrappers.py
@@ -15,7 +15,69 @@ if __name__ == '__main__':
     lua_code = "function registerCodeEditorInfos(m, x) print(x) end; require 'sim-2-ce'"
     proc = subprocess.Popen(["lua", "-e", lua_code], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-    gen_code = 'return {extend = function(sim)\n\n'
+    gen_code = """
+local function write_vector3(v)
+    return Vector(v)
+end
+
+local function write_matrix(v)
+    return Matrix(v)
+end
+
+local function write_quaternion(v)
+    return Quaternion(v)
+end
+
+local function write_pose(v)
+    return Pose(v)
+end
+
+local function write_color(v)
+    return Color(v)
+end
+
+local function write_handle(v)
+    return sim.Object(v)
+end
+
+local function read_vector3(v, def)
+    if v == nil then v = def end
+    if Vector:isvector(v) then v = v:data() end
+    return v
+end
+
+local function read_matrix(v, def)
+    if v == nil then v = def end
+    if Matrix:ismatrix(v) then v = v:data() end
+    return v
+end
+
+local function read_quaternion(v, def)
+    if v == nil then v = def end
+    if Quaternion:isquaternion(v) then v = v:data() end
+    return v
+end
+
+local function read_pose(v, def)
+    if v == nil then v = def end
+    if Pose:ispose(v) then v = v:data() end
+    return v
+end
+
+local function read_color(v, def)
+    if v == nil then v = def end
+    if Color:iscolor(v) then v = v:data() end
+    return v
+end
+
+local function read_handle(v, def)
+    if v == nil then v = def end
+    if sim.Object:isobject(v) then v = #v end
+    return v
+end
+
+return {extend = function(sim)\n
+"""
     for line in proc.stdout:
         calltip = line.strip()
         if not calltip: continue
@@ -27,44 +89,28 @@ if __name__ == '__main__':
             in_recipes, out_recipes = {}, {}
             for i, arg in enumerate(f.in_args):
                 if isinstance(arg, VarArgs): continue
-                if arg.type == 'vector3':
-                    in_recipes[i] = 'Vector:tovector'
-                elif arg.type == 'matrix':
-                    in_recipes[i] = 'Matrix:tomatrix'
-                elif arg.type == 'quaternion':
-                    in_recipes[i] = 'Quaternion:toquaternion'
-                elif arg.type == 'pose':
-                    in_recipes[i] = 'Pose:topose'
-                elif arg.type == 'color':
-                    in_recipes[i] = 'Color:tocolor'
-                elif arg.type == 'handle':
-                    in_recipes[i] = 'sim.Object:toobject'
+                if arg.type in ('vector3', 'matrix', 'quaternion', 'pose', 'color', 'handle'):
+                    in_recipes[i] = arg.type
             for i, ret in enumerate(f.out_args):
                 if isinstance(ret, VarArgs): continue
-                if ret.type == 'vector3':
-                    out_recipes[i] = 'Matrix.data'
-                elif ret.type == 'matrix':
-                    out_recipes[i] = 'Matrix.data'
-                elif ret.type == 'quaternion':
-                    out_recipes[i] = 'Quaternion.data'
-                elif ret.type == 'pose':
-                    out_recipes[i] = 'Pose.data'
-                elif ret.type == 'color':
-                    out_recipes[i] = 'Color.data'
-                elif ret.type == 'handle':
-                    out_recipes[i] = '#'
+                if ret.type in ('vector3', 'matrix', 'quaternion', 'pose', 'color', 'handle'):
+                    out_recipes[i] = ret.type
             if not in_recipes and not out_recipes: continue
             gen_code += f"""{f.func_name} = wrap({f.func_name}, function(origFunc)
     return function(...)
         local args = {{...}}
 """
             for i, recipe in in_recipes.items():
-                gen_code += f"""        args[{i}] = {recipe}(args[{i}]) -- {f.in_args[i].name} [{f.in_args[i].type}]
+                dv = 'nil'
+                if hasattr(f.in_args[i], 'default'):
+                    dv = str(f.in_args[i].default)
+                    dv = dv.replace('[', '{').replace(']', '}')
+                gen_code += f"""        args[{i}] = read_{recipe}(args[{i}], {dv}) -- {f.in_args[i].name} [{f.in_args[i].type}]
 """
-            gen_code += f"""        local ret = {origFunc(table.unpack(args))}
+            gen_code += f"""        local ret = {{origFunc(table.unpack(args))}}
 """
             for i, recipe in out_recipes.items():
-                gen_code += f"""        ret[{i}] = {recipe}(ret[{i}]) -- {f.out_args[i].name} [{f.out_args[i].type}]
+                gen_code += f"""        ret[{i}] = write_{recipe}(ret[{i}]) -- {f.out_args[i].name} [{f.out_args[i].type}]
 """
             gen_code += f"""        return table.unpack(ret)
     end
