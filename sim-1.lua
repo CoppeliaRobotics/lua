9f8cd459b41ad9ba3269f1d29e48433525a1504b
Federico Ferri <federico.ferri.it@gmail.com>
2025-08-29 13:58:11 +0200

    move apropos() to utils/; search in objects' properties as well

--- sim-1.lua
+++ sim-1.lua
@@ -1617,30 +1617,7 @@ function sim.setShapeAppearance(handle, savedData, opts)
 end
 
 apropos = apropos or function(what) -- other sim-versions also have a global apropos function...
-    what = what:lower()
-    local mods = {sim = sim}
-    for i, n in ipairs(sim.getLoadedPlugins()) do
-        pcall(function() mods[n] = require(n) end)
-    end
-    local results = {}
-    for n, m in pairs(mods) do
-        for k, v in pairs(m) do
-            if k:lower():match(what) then
-                local s = n .. '.' .. k
-                local info = s
-                if type(v) == 'function' then
-                    info = s .. '(...)'
-                    local i = sim.getApiInfo(-1, s)
-                    if i and i ~= '' then info = (string.split(i, '\n'))[1] end
-                end
-                table.insert(results, {s, info})
-            end
-        end
-    end
-    table.sort(results, function(a, b) return a[1] < b[1] end)
-    local s = ''
-    for i, result in ipairs(results) do s = s .. (s == '' and '' or '\n') .. result[2] end
-    print(s)
+    utils.apropos(what)
 end
 
 -- wrap require() to load embedded scripts' code when called with a script handle, e.g. require(sim.getObject '/foo')
