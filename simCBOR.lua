1e3fa7cf9fd383b6b3c1f5cb66c2d4cea401ef74
Federico Ferri <federico.ferri.it@gmail.com>
2026-02-06 08:59:27 +0100

    handle CBOR RFC8746 typed-array tags 64/69/70/71/78/79/85/86

--- simCBOR.lua
+++ simCBOR.lua
@@ -18,6 +18,46 @@ function type_tags.TAG_40(values)
     return simEigen.Matrix(rows, cols, data)
 end
 
+-- RFC8746 typed-arrays: uint8 Typed Array
+function type_tags.TAG_64(values)
+    return sim.unpackUInt8Table(values)
+end
+
+-- RFC8746 typed-arrays: uint16, little endian, Typed Array
+function type_tags.TAG_69(values)
+    return sim.unpackUInt16Table(values)
+end
+
+-- RFC8746 typed-arrays: uint32, little endian, Typed Array
+function type_tags.TAG_70(values)
+    return sim.unpackUInt32Table(values)
+end
+
+-- RFC8746 typed-arrays: uint64, little endian, Typed Array
+function type_tags.TAG_71(values)
+    return sim.unpackUInt64Table(values)
+end
+
+-- RFC8746 typed-arrays: sint32, little endian, Typed Array
+function type_tags.TAG_78(values)
+    return sim.unpackInt32Table(values)
+end
+
+-- RFC8746 typed-arrays: sint64, little endian, Typed Array
+function type_tags.TAG_79(values)
+    return sim.unpackInt64Table(values)
+end
+
+-- RFC8746 typed-arrays: IEEE 754 binary32, little endian, Typed Array
+function type_tags.TAG_85(values)
+    return sim.unpackFloatTable(values)
+end
+
+-- RFC8746 typed-arrays: IEEE 754 binary64, little endian, Typed Array
+function type_tags.TAG_86(values)
+    return sim.unpackDoubleTable(values)
+end
+
 function type_tags.TAG_4294970000(value)
     local Color = require 'Color'
     return Color(value)
