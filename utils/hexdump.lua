37475a6964e8bce460e9c96af862af5a3e5a14f0
Federico Ferri <federico.ferri.it@gmail.com>
2026-03-30 18:03:29 +0200

    fix a bug in utils.hexdump(), optimize, change output of nonprintable bytes to '.'

--- utils/hexdump.lua
+++ utils/hexdump.lua
@@ -4,26 +4,31 @@ return function(x, opts)
     opts.offset = opts.offset ~= false
     opts.printable = opts.printable ~= false
     assert(isbuffer(x) or type(x) == 'string', 'works only on buffer and strings')
-    local s = ''
-    local sep = ''
+    local s = {}
     local ow = #x > 0 and math.floor(math.log(#x, 16)) + 1 or 1
+
+    local byteHex, byteChar = {}, {}
+    for i = 0, 255 do
+        byteHex[i] = string.format(' %02x', i)
+        byteChar[i] = ' ' .. (i >= 32 and i < 127 and string.char(i) or '.')
+    end
+    byteHex[-1] = '   '
+    byteChar[-1] = ''
+
     for i = 1, #x, opts.width do
         if opts.offset then
-            s = s .. string.format(' %0' .. ow .. 'x |', i - 1)
+            table.insert(s, string.format(' %0' .. ow .. 'x |', i - 1))
         end
         for j = i, i + opts.width - 1 do
-            local c = x:sub(j, j)
-            s = s .. (c == '' and '   ' or string.format(' %02x', string.byte(c)))
+            table.insert(s, byteHex[x:byte(j) or -1])
         end
         if opts.printable then
-            s = s .. ' |'
-            for j = i, math.min(#x, i + opts.width) do
-                local c = x:sub(j, j)
-                s = s .. ' ' .. (string.isprintable(c) and c or ' ')
+            table.insert(s, ' |')
+            for j = i, math.min(#x, i + opts.width - 1) do
+                table.insert(s, byteChar[x:byte(j) or -1])
             end
         end
-        s = s .. '\n'
-        sep = ' '
+        table.insert(s, '\n')
     end
-    print(s)
+    print(table.concat(s))
 end
