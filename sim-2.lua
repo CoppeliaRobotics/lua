b8551544217f96895f413a2e50ae39f6bf7b2cc2
Coppelia <mfreese@gmx.ch>
2026-04-02 07:23:54 +0200

    fix sim.callMethod for custom methods

--- sim-2.lua
+++ sim-2.lua
@@ -10,19 +10,16 @@ local checkargs = require('checkargs-2')
 require('motion-2').extend(sim)
 
 function sim.callMethod(target, name, ...)
-    local h = target
-    if sim.Object:isobject(h) then
-        h = h.handle
+    local targetHandle = target
+    if type(target) ~= 'number' then
+        targetHandle = target.__handle -- cannot call target.handle, target:isValid, etc.
     end
 
     -- handling of custom methods
-    if h >= sim.object_customstart and h <= sim.object_customend then
-        local t = callMethod(sim.app, 'getCustomObjectType', target)
-        if t then
-            local m = CustomClass:getMethod(t, name)
-            if m then
-                return m(...)
-            end
+    if targetHandle >= sim.object_customstart and targetHandle <= sim.object_customend and callMethod(targetHandle, 'isValid') then
+        local m = CustomClass:getMethod(callMethod(targetHandle, 'getStringProperty', 'objectType'), name)
+        if m then
+            return m(...) -- calling custom Lua method
         end
         -- custom method not found, continue with searching standard methods below:
     end
@@ -60,91 +57,10 @@ function sim.callMethod(target, name, ...)
                 return err -- error msg
             end
         else
-            return locals[name](target, name, ...)
+            return locals[name](target, name, ...) -- calling built-in Lua method
         end
     else
-        if sim.Object:isobject(target) then
-            target = h
-        end
-        return callMethod(target, name, ...)
-        --[[
-        function toSimpleType(arg)
-            local t = -1 -- stands for simple types directly supported (e.g. sim.stackitem_double, sim.stackitem_table, etc.)
-            if arg == nil then
-                t = sim.stackitem_null
-                arg = 0
-            elseif type(arg) == 'table' then
-                if isbuffer(arg) then
-                    t = -2 -- stands for buffer
-                    arg = tostring(arg)
-                elseif sim.Object:isobject(arg) then
-                    t = sim.stackitem_handle
-                    arg = arg.handle
-                elseif simEigen.Matrix:ismatrix(arg) then
-                    t = 'm' .. tostring(arg:rows()) .. 'x' .. tostring(arg:cols()) -- "m[rows]x[cols]"
-                    arg = arg:data()
-                elseif simEigen.Quaternion:isquaternion(arg) then
-                    t = sim.stackitem_quaternion
-                    arg = arg:data()
-                elseif simEigen.Pose:ispose(arg) then
-                    t = sim.stackitem_pose
-                    arg = arg:data()
-                else
-                    local narg = {}
-                    t = {}
-                    for k, v in pairs(arg) do
-                        local arg_, t_ = toSimpleType(v)
-                        narg[k] = arg_
-                        t[k] = t_
-                    end
-                    arg = narg
-                end
-            end
-            return arg, t
-        end
-
-        function toExtendedType(arg, t)
-            if t == -2 then
-                arg = tobuffer(arg)
-            elseif t == sim.stackitem_null then
-                arg = nil
-            elseif t == sim.stackitem_handle then
-                arg = sim.Object(arg)
-            elseif t == sim.stackitem_quaternion then
-                arg = simEigen.Quaternion(arg)
-            elseif t == sim.stackitem_pose then
-                arg = simEigen.Pose(arg)
-            elseif type(t) == 'string' then
-                local rows, cols = t:match("m(%d+)x(%d+)") -- "m[rows]x[cols]"
-                arg = simEigen.Matrix(tonumber(rows), tonumber(cols), arg)
-            elseif type(t) == 'table' then
-                local narg = {}
-                for k, v in pairs(arg) do
-                    local arg_ = toExtendedType(v, t[k])
-                    narg[k] = arg_
-                end
-                arg = narg
-            end
-            return arg
-        end
-
-        local args = table.pack(...)
-        local types = {}
-        for i = 1, args.n do
-            local arg, t = toSimpleType(args[i])
-            args[i] = arg
-            types[i] = t
-        end
-        args.n = nil -- important!!
-        local retVals = {}
-        local ret = table.pack(callMethod(target, name, args, types))
-        ret.n = nil
-        for i = 1, #ret // 2 do
-            local arg = toExtendedType(ret[2 * (i - 1) + 1], ret[2 * (i - 1) + 2])
-            retVals[i] = arg
-        end
-        return table.unpack(retVals)
-        --]]
+        return callMethod(target, name, ...) -- calling c method
     end
 end
 
