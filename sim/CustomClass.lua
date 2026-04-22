eec4c36d99b34a46e1884569701f803c317b34d9
Federico Ferri <federico.ferri.it@gmail.com>
2026-04-22 17:51:04 +0200

    sim.CustomClass fix for method property write

--- sim/CustomClass.lua
+++ sim/CustomClass.lua
@@ -1,9 +1,30 @@
-return function(name, opts)
+local class = require 'middleclass'
+
+local CustomClass = class 'sim.CustomClass'
+
+function CustomClass:initialize(name, opts)
     local sim = require 'sim-2'
     for i, c in ipairs(sim.app.customClasses) do
         if c.objectType == name then
-            return c
+            rawset(self, '__class', c)
+            return
         end
     end
-    return sim.Object(sim.app:createCustomObject(name, opts or {classMetaInfo = '{"superclass": "object"}'}))
+    rawset(self, '__class', sim.Object(sim.app:createCustomObject(name, opts or {classMetaInfo = '{"superclass": "object"}'})))
 end
+
+function CustomClass:__index(k)
+    local v = self.__class[k]
+    if type(v) == 'function' then
+        return function(_, ...) return v(self.__class, ...) end
+    end
+    return v
+end
+
+function CustomClass:__newindex(k, v)
+    if type(v) == 'function' then
+        self.__class:setMethodProperty(k, v)
+    end
+end
+
+return CustomClass
