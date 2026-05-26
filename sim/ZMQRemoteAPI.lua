a87c6363a41c629c9a5a545acc5a68a170dc55f3
Federico Ferri <federico.ferri.it@gmail.com>
2026-05-26 13:57:45 +0200

    revert to REQ/REP

--- sim/ZMQRemoteAPI.lua
+++ sim/ZMQRemoteAPI.lua
@@ -2,8 +2,9 @@ local class = require 'middleclass'
 local sim = require 'sim-2'
 local simCBOR = require 'simCBOR'
 local simZMQ = require 'simZMQ'
-local uuid = require 'uuid'
 simZMQ.__raiseErrors()
+local uuid = require 'uuid'
+uuid.set_rng(uuid.rng.math_random())
 
 local ZMQRemoteAPI = class 'sim.ZMQRemoteAPI'
 
@@ -12,13 +13,12 @@ function ZMQRemoteAPI:initialize(opts)
     self.name = opts.name
     self.server = not not opts.server
     self.verbose = tonumber(opts.verbose or 0)
-
-    self.clientID = math.random(1 << 10, 1 << 24)
-    self.pendingRequests = {}
+    self.clientID = opts.clientID or uuid.v4()
     local ctx = simZMQ.ctx_singleton()
     local host = opts.host or '127.0.0.1'
     local port = opts.port or 24020
-    self.socket = simZMQ.socket(ctx, simZMQ[opts.socketType or 'DEALER'])
+    local socketType = self.server and 'REP' or 'REQ'
+    self.socket = simZMQ.socket(ctx, simZMQ[socketType])
     if opts.server then
         simZMQ.bind(self.socket, 'tcp://*:' .. port)
     else
@@ -26,117 +26,82 @@ function ZMQRemoteAPI:initialize(opts)
     end
 end
 
-function ZMQRemoteAPI:__gc()
+function ZMQRemoteAPI:cleanup()
     if self.socket then
         simZMQ.close(self.socket)
         self.socket = nil
     end
 end
 
+function ZMQRemoteAPI:__gc()
+    self:cleanup()
+end
+
 function ZMQRemoteAPI:log(level, ...)
     if level <= self.verbose then
-        local id = string.format('ZMQRemoteAPI[%s%sid=%d]', self.name or '', self.name and ', ' or '', self.clientID)
+        local id = 'ZMQRemoteAPI'
+        if self.name then id = id .. '[' .. self.name .. ']' end
         print(id, ...)
     end
 end
 
-function ZMQRemoteAPI:generateRequestID()
-    local id = self.nextRequestID or 1000
-    self.nextRequestID = id + 1
-    return id
-end
-
 function ZMQRemoteAPI:callLocal(funcName, args, successCallback, errorCallback)
-    local ok, result = pcall(function()
-        assert(type(funcName) == 'string', 'invalid function name')
-        local func = _G[funcName]
-        assert(func, 'no such function: ' .. funcName)
-        assert(type(func) == 'function', 'not a function: ' .. funcName)
-        args = args or {}
-        assert(type(args) == 'table', 'invalid args')
-        return table.pack(func(table.unpack(args or {})))
-    end)
-    if ok then
-        successCallback(result)
-    else
-        errorCallback(result)
-    end
+    assert(type(funcName) == 'string', 'invalid function name')
+    local func = _G[funcName]
+    assert(func, 'no such function: ' .. funcName)
+    assert(type(func) == 'function', 'not a function: ' .. funcName)
+    args = args or {}
+    assert(type(args) == 'table', 'invalid args')
+    return table.pack(func(table.unpack(args or {})))
 end
 
-function ZMQRemoteAPI:callRemote(funcName, args, successCallback, errorCallback)
-    local id = self:generateRequestID()
-    self.pendingRequests[id] = {
-        successCallback = successCallback,
-        errorCallback = errorCallback,
-    }
-    self:send{
-        client_id = self.clientID,
-        req_id = id,
-        msg = 'call',
-        func = funcName,
-        args = args,
-    }
+function ZMQRemoteAPI:call(funcName, ...)
+    assert(type(funcName) == 'string', 'invalid function name')
+    local args = table.pack(...)
+    self:send{msg = 'call', func = funcName, args = args}
+    while true do
+        local rep = self:recv(true)
+        if rep.msg == 'result' then
+            if rep.error then
+                error(rep.result)
+            else
+                return table.unpack(rep.result)
+            end
+        else
+            self:handleRequest(rep)
+        end
+    end
 end
 
 function ZMQRemoteAPI:registerCallbackLocal(funcName)
     assert(type(funcName) == 'string', 'invalid function name')
     _G[funcName] = function(...)
-        self:callLocal(funcName, ...)
+        return self:call(funcName, ...)
     end
 end
 
-function ZMQRemoteAPI:registerCallbackRemote(funcName)
-    self:send{
-        client_id = self.clientID,
-        msg = 'registerCallback',
-        func = funcName,
-    }
+function ZMQRemoteAPI:registerCallback(funcName)
+    self:send{msg = 'registerCallback', func = funcName}
+    local rep = self:recv(true)
+    assert(rep.msg == 'result', 'invalid server reply')
+    assert(not rep.error, 'registerCallback failed')
 end
 
 function ZMQRemoteAPI:handleRequest(req)
     assert(type(req.msg) == 'string', 'malformed request')
-    assert(math.type(req.client_id) == 'integer', 'malformed request')
     if req.msg == 'call' then
-        assert(math.type(req.req_id) == 'integer', 'malformed request')
-        self:callLocal(req.func, req.args,
-            function(result)
-                self:send{
-                    msg = 'result',
-                    client_id = req.client_id,
-                    req_id = req.req_id,
-                    error = false,
-                    result = result,
-                }
-            end,
-            function(err)
-                self:send{
-                    msg = 'result',
-                    client_id = req.client_id,
-                    req_id = req.req_id,
-                    error = true,
-                    result = err,
-                }
-            end
-        )
+        local ok, result = pcall(self.callLocal, self, req.func, req.args)
+        self:send{msg = 'result', error = not ok, result = result}
     elseif req.msg == 'registerCallback' then
-        self:registerCallbackLocal(req.func)
-    elseif req.msg == 'result' then
-        if self.pendingRequests[req.req_id] then
-            if req.error then
-                self.pendingRequests[req.req_id].errorCallback(req.result)
-            else
-                self.pendingRequests[req.req_id].successCallback(req.result)
-            end
-            self.pendingRequests[req.req_id] = nil
-        else
-            self:log(1, 'got result without matching request id ' .. req.req_id)
-        end
+        local ok, result = pcall(self.registerCallbackLocal, self, req.func)
+        self:send{msg = 'result', error = not ok, result = result}
     else
         self:log(1, 'unsupported message:', req.msg)
     end
 end
 
 function ZMQRemoteAPI:handleRequests()
+    assert(self.server, 'handleRequests should be called only from server')
     while true do
         local req = self:recv()
         if not req then break end
@@ -145,22 +110,28 @@ function ZMQRemoteAPI:handleRequests()
 end
 
 function ZMQRemoteAPI:send(msg)
+    assert(self.socket)
     assert(type(msg) == 'table', 'bad type')
     self:log(2, 'sending:', msg)
     local data = simCBOR.encode(msg)
     simZMQ.send(self.socket, data, 0)
+    self:log(2, 'sent')
 end
 
-function ZMQRemoteAPI:recv()
-    local r, data = simZMQ.__noError.recv(self.socket, simZMQ.NOBLOCK)
+function ZMQRemoteAPI:recv(block)
+    assert(self.socket)
+    if block then
+        self:log(2, 'receiving... (block)')
+    end
+    local r, data = simZMQ.recv(self.socket, block and 0 or simZMQ.NOBLOCK)
     if r == -1 then return end
     local ok, req = pcall(simCBOR.decode, data)
-    if ok then
-        self:log(2, 'received:', req)
-        return req
-    else
+    if not ok then
         self:log(1, 'invalid request CBOR data')
+        return
     end
+    self:log(2, 'received:', req)
+    return req
 end
 
 return ZMQRemoteAPI
