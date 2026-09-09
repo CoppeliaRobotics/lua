e7da01d0bc0c559eaa8b3c9add26bbd3a172fe31
Federico Ferri <federico.ferri.it@gmail.com>
2026-09-09 11:39:01 +0200

    module 'sim' to also load matrixLazyLoaders

--- sim.lua
+++ sim.lua
@@ -1,1 +1,4 @@
-return require 'sim-1'
+-- this module is the one autoloaded (when using sim.xxx without explicit require)
+local sim = require 'sim-1'
+require 'deprecated.matrixLazyLoaders'
+return sim
